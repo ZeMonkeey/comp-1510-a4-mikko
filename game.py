226a3fd1ebf7cd2488e29848854a15db9d6805b0
Mikko Sio <msio1@my.bcit.ca>
2022-12-03 00:22:46 -0800

    Completed get_pokeball function

--- game.py
+++ game.py
@@ -5,6 +5,7 @@ A01331396
 
 
 import time
+import random
 
 
 def make_board(rows: int, columns: int) -> dict:
@@ -14,7 +15,6 @@ def make_board(rows: int, columns: int) -> dict:
             board[row, column] = "|   |"
     board[2, 2] = "| x |"
     board[4, 4] = "|Gym|"
-    board[4, 1] = "| S |"
     board[4, 2] = "| H |"
     pokeballs = [(4, 0), (0, 2)]
     grass_patch = [(0, 0), (0, 1), (1, 0), (1, 1)]
@@ -81,11 +81,12 @@ def make_trainer(name: str) -> dict:
     time.sleep(1)
     print("You may now start your journey on becoming the Pokemon champion!")
     time.sleep(1)
-    trainer_info = {"name": name, "pokemons": [pokemon_choice]}
+    trainer_info = {"name": name, "pokemons": [pokemon_choice], "inventory": {"pokeball": 5, "potion": 5, "revive": 0}}
     return trainer_info
 
 
-def get_user_choice() -> str:
+def get_user_choice(trainer, board) -> tuple:
+    trainer_position = [coordinate for coordinate, value in board.items() if "x" in value][0]
     directions = ("Up", "Down", "Left", "Right")
     choices = ["1", "2", "3", "4"]
     choice = None
@@ -93,19 +94,31 @@ def get_user_choice() -> str:
         print(f"{sequence_number}: {direction}")
     while choice not in choices:
         choice = input("Enter a number to move (1-4): ")
-    return choice
+    if choice == "1":
+        return trainer_position[0], trainer_position[1] - 1
+    elif choice == "2":
+        return trainer_position[0], trainer_position[1] + 1
+    elif choice == "3":
+        return trainer_position[0] - 1, trainer_position[1]
+    elif choice == "4":
+        return trainer_position[0] + 1, trainer_position[1]
 
 
-def validate_move(board: dict, direction: str) -> bool:
+def validate_move(board: dict, direction: tuple) -> bool or str:
+    if direction == board[(4, 0)] or direction == board[(0, 2)]:
+        return "pokeball"
+    elif direction == board[(4, 2)]:
+        return "heal"
+    elif direction == board[(4, 4)]:
+        return "gym"
+
     trainer_position = [coordinate for coordinate, value in board.items() if "x" in value][0]
-    surrounding_spaces = {(trainer_position[0] - 1, trainer_position[1]): "3",
-                          (trainer_position[0] + 1, trainer_position[1]): "4",
-                          (trainer_position[0], trainer_position[1] - 1): "1",
-                          (trainer_position[0], trainer_position[1] + 1): "2"}
-    valid_choices = []
-    for coordinate, value in surrounding_spaces.items():
-        if coordinate in board.keys():
-            valid_choices.append(value)
+    surrounding_spaces = ((trainer_position[0] - 1, trainer_position[1]),
+                          (trainer_position[0] + 1, trainer_position[1]),
+                          (trainer_position[0], trainer_position[1] - 1),
+                          (trainer_position[0], trainer_position[1] + 1))
+    events = ("| p |", "| S |", "| H |", "|Gym|")
+    valid_choices = [coordinate for coordinate in surrounding_spaces if coordinate in board.keys()]
     if direction in valid_choices:
         return True
     return False
@@ -114,20 +127,30 @@ def validate_move(board: dict, direction: str) -> bool:
 def move_trainer(board, direction):
     trainer_position = [coordinate for coordinate, value in board.items() if "x" in value][0]
     board[trainer_position] = board[trainer_position].replace("x", " ")
-    space_to_move_into = None
-    if direction == "1":
-        space_to_move_into = (trainer_position[0], trainer_position[1] - 1)
-    elif direction == "2":
-        space_to_move_into = (trainer_position[0], trainer_position[1] + 1)
-    elif direction == "3":
-        space_to_move_into = (trainer_position[0] - 1, trainer_position[1])
-    elif direction == "4":
-        space_to_move_into = (trainer_position[0] + 1, trainer_position[1])
-    display_of_space = list(board[space_to_move_into])
+    display_of_space = list(board[direction])
     display_of_space[2] = "x"
     display_of_space = "".join(display_of_space)
-    board[space_to_move_into] = display_of_space
+    board[direction] = display_of_space
     return board
+
+
+def get_pokeball(trainer):
+    possible_items = ("potion", "potion", "pokeball", "pokeball", "pokeball", "revive")
+    print("There is a pokeball there!")
+    print("You pick up the pokeball and open it.")
+    reward = possible_items[random.randint(0, 5)]
+    for item, value in trainer["inventory"]:
+        if reward == item:
+            value += 1
+    return trainer
+
+
+def heal_pokemon(trainer):
+    pass
+
+
+def fight_gym(trainer):
+    pass
 
 
 def check_for_events(board):
@@ -145,9 +168,15 @@ def game():  # called from main
         # Tell the user where they are
         display_board(board)
         time.sleep(1)
-        direction = get_user_choice()
+        direction = get_user_choice(trainer, board)
         valid_move = validate_move(board, direction)
-        if valid_move:
+        if valid_move == "pokeball":
+            get_pokeball(trainer)
+        elif valid_move == "heal":
+            heal_pokemon(trainer)
+        elif valid_move == "gym":
+            fight_gym(trainer)
+        elif valid_move:
             move_trainer(board, direction)
             display_board(board)
             event_type = check_for_events(board)
@@ -155,14 +184,12 @@ def game():  # called from main
                 execute_challenge_protocol(trainer)
             elif event_type == "water":
                 pass
-            elif event_type == "pokeball":
-                pass
 
                 # if trainer_has_leveled():
                 #     execute_glow_up_protocol()
             # achieved_goal = check_if_goal_attained(board, trainer)
         else:
-            print("Not a valid move! Pick again.")
+            print("That's not a valid move! Please pick again")
             time.sleep(1)
 
 
